110210369037306a6ef0af3250d127d6f1fd225f
satosh <smartdev.house108@gmail.com>
2023-04-30 19:28:07 -0400

    Add GiftCard model, admin

--- watermelon_seller_survey/giftcard/admin.py
+++ watermelon_seller_survey/giftcard/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import GiftCard
 
-# Register your models here.
+
+@admin.register(GiftCard)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'survey_id',
+        'store_name',
+        'amount',
+        'price',
+        'accepts_crypto',
+        'crypto_network',
+        'wallet_address',
+        'deposit_or_validate',
+        'email')
